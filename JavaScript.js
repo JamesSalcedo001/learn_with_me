dfe402ee922c5351e9a7abd37ad2d1a60a7c598e
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-02-08 14:30:09 -0600

    added starting code, concat, and charAt in JavaScript file

---
+++ JavaScript.js
@@ -0,0 +1,26 @@
+// String Methods
+
+// ***
+
+
+// charAt(index_number) ==> returns character at specified index in a string
+
+
+let text1 = "Hello World!"
+console.log("character at index 2 ==> ", text1.charAt(2))
+
+
+
+console.log("\n***\n")
+// concat(target_string_to_add) ==> joins two or more strings, returns new string, non destructive
+
+
+
+let string1 = "Hello"
+let string2 = " World!"
+
+console.log("hello + world joined ==> ", string1.concat(string2))
+
+
+
+console.log("\n***\n")
